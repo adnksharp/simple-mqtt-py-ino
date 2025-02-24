58fd7611264fa0f325866fc9fe635f2dfaf904cc
adnksharp <darkmark404@protonmail.com>
2025-02-23 21:12:10 -0700

    chore: init mqtt class for robot node

--- nodes/robot/main.py
+++ nodes/robot/main.py
@@ -1,29 +1,47 @@
 from paho.mqtt import client
 from numpy import cos, sin, array, dot
-import tabulate.tabulate as tab
+from tabulate import tabulate
+import json
+
+def calc(client, userdata, msg):
+    topic = str(msg.topic)
+    data = json.loads(msg.payload.decode('utf-8'))
+    print(topic, data)
 
 class MQTT():
-    def __init__(self, broker = 'localhost', port, ka):
-        topics: list = []
-        clients: list = []
-        publish: list = []
+    def __init__(self, broker = 'localhost', port = 8080, ka = 60):
+        self.clients: list = []
+        self.publish: list = []
         self.broker = str(broker)
         self.port = int(port)
         self.ka = int(ka)
-        self.quos = 0
+        self.qos = 0
 
     def config(self, version, sub = False, topic = None):
-        self.clients.append(client(version))
-        self.publish.append(None)
+        if not topic:
+            return
+        if sub:
+            self.clients.append(client.Client(version))
+            self.clients[-1].on_message = calc
+            self.clients[-1].connect(self.broker, self.port, self.ka)
+            self.clients[-1].subscribe(str(topic), qos = self.qos)
+            self.qos += 1
 
-        self.clinets[-1].connect(self.broker, self.port, self.ka)
-        if topic:
-            if sub:
-                self.clients[-1].subscribe(str(topic), quos = self.quos)
-                self.quos += 1
-            else:
-                self.pusblish[-1] = str(topic)
+        else:
+            self.publish.append(None)
+            self.publish[-1] = str(topic)
 
     def exec(self):
         for client in self.clients:
-            client.toop_forever()
+            client.loop_forever()
+
+if __name__ == '__main__':
+    mqttver = client.CallbackAPIVersion.VERSION2
+    mosquitto = MQTT(broker = 'localhost', port = 1883, ka = 60)
+    mosquitto.config(version = mqttver, sub = True, topic = 'robot/velocidad/izquierda')
+    mosquitto.config(version = mqttver, sub = True, topic = 'robot/velocidad/derecha')
+    mosquitto.config(version = mqttver, sub = False, topic = 'robot/posicion')
+    try:
+        mosquitto.exec()
+    except:
+        pass
