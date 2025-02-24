2f738f23c1e1ab5c96647251337a98162b7c141e
adnksharp <darkmark404@protonmail.com>
2025-02-24 02:19:02 -0700

    feat: mathematical class for robot node configured

--- nodes/robot/main.py
+++ nodes/robot/main.py
@@ -4,13 +4,52 @@ from tabulate import tabulate
 import json
 
 class Mathematize():
-    def __init__(self):
-        pass
+    def __init__(self, R = 0.02, L = 0.1):
+        self.x, self.y, self.theta = 0.0, 0.0, 0.0
+        self.data:list = [0.0, 0.0]
+        self.R, self.L = R, L
+        self.last = None
+
+    def getVel(self):
+        v = self.data
+        T = array([
+            [self.R/2 * cos(self.theta), self.R / 2 * cos(self.theta)],
+            [self.R/2 * sin(self.theta), self.R / 2 * sin(self.theta)],
+            [self.R/self.L , -self.R/self.L]
+        ])
+
+        return dot(T, v).tolist()
+
+    def getPos(self, v, dt):
+        vx, vy, vt = v
+
+        return [self.x + vx * dt, self.y + vy * dt, self.theta + vt * dt]
+
+    def index(self, topic, data):
+        if topic == 'robot/velocidad/derecha':
+            self.data[0] = data['read']
+        elif topic == 'robot/velocidad/izquierda':
+            self.data[1] = data['read']
+
+    def getdt(self, millis):
+        if self.last is None:
+            self.last = millis
+            return None
+        dt = (millis - self.last) / 1000.0
+        self.last = millis
+
+        return dt
 
     def get(self, client, userdata, msg):
         topic = str(msg.topic)
         data = json.loads(msg.payload.decode('utf-8'))
-        print(topic, data)
+        self.index(topic, data)
+        dt = self.getdt(float(data['time']))
+        if dt is None:
+            return
+        self.x, self.y, self.theta = self.getPos(self.getVel(), dt)
+        print(tabulate([self.data, [self.x, self.y, self.theta]], floatfmt='.5f', tablefmt='simple_grid'))
+
 
 class MQTT():
     def __init__(self, version, broker = 'localhost', port = 8080, ka = 60, f = None):
@@ -34,16 +73,17 @@ class MQTT():
             self.publish.append(str(topic))
 
     def exec(self):
-        self.host.loop_forever()
+        try:
+            self.host.loop_forever()
+        except:
+            pass
+
 
 if __name__ == '__main__':
     mqttver = client.CallbackAPIVersion.VERSION2
-    math = Mathematize()
+    math = Mathematize(R = 0.5, L = 0.1)
     mosquitto = MQTT(version = mqttver, broker = 'localhost', port = 1883, ka = 60, f = math.get)
     mosquitto.config(sub = True, topic = 'robot/velocidad/izquierda')
     mosquitto.config(sub = True, topic = 'robot/velocidad/derecha')
     mosquitto.config(sub = False, topic = 'robot/posicion')
-    try:
-        mosquitto.exec()
-    except:
-        pass
+    mosquitto.exec()
